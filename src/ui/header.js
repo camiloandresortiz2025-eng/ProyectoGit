96383edb47e41095bf7d4616f1e233e1daf75747
Camilo Ortiz <camiloandresortiz2025@gmail.com>
2026-09-21 20:58:57 -0500

    feat: agregar navegacion al componente header

--- src/ui/header.js
+++ src/ui/header.js
@@ -1,10 +1,12 @@
 // Componente de interfaz de usuario
 // Funcionalidad: Header
 
-function renderHeader(tituloProyecto) {
+function renderHeader(tituloProyecto, navLinks = []) {
+  const nav = navLinks.map((link) => `<a href="${link.url}">${link.label}</a>`).join('');
   return `
     <header>
       <h1>${tituloProyecto}</h1>
+      <nav>${nav}</nav>
     </header>
   `;
 }
